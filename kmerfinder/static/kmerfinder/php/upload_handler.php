67c8cfd7cc502718c8115c0c00a68f016a3e5fda
hvlarsen <hansvoldbylarsen@gmail.com>
2016-03-01 19:55:59 +0100

    changed upload_handler

--- kmerfinder/static/kmerfinder/php/upload_handler.php
+++ kmerfinder/static/kmerfinder/php/upload_handler.php
@@ -1,5 +1,5 @@
 <?php
 var_dump($_FILES);
 $gs_name = $_FILES['upl']['name'];
-move_uploaded_file($gs_name, 'gs://my_bucket/new_file.txt');
+move_uploaded_file($gs_name, 'gs://kmerfindercloud.appspot.com/new_file.txt');
 ?>
