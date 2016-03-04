2724f24c83704b229950e47f3384edcb972d1b1b
hvlarsen <hansvoldbylarsen@gmail.com>
2016-03-04 19:36:47 +0100

    tester

--- kmerfinder/static/kmerfinder/php/upload.php
+++ kmerfinder/static/kmerfinder/php/upload.php
@@ -2,7 +2,16 @@
 use google\appengine\api\cloud_storage\CloudStorageTools;
 $options = [ 'gs_bucket_name' => 'kmerfindercloud.appspot.com' ];
 $upload_url = CloudStorageTools::createUploadUrl('/upload_handler.php', $options);
+function debug_to_console( $data ) {
 
+    if ( is_array( $data ) )
+        $output = "<script>console.log( 'Debug Objects: " . implode( ',', $data) . "' );</script>";
+    else
+        $output = "<script>console.log( 'Debug Objects: " . $data . "' );</script>";
+
+    echo $output;
+}
+debug_to_console( "Test" );
 // A list of permitted file extensions
 $allowed = array('png', 'jpg', 'gif','zip');
 
